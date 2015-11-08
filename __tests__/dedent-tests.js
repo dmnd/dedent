4e5f97cbdb952cf078b40315f5219081605fda8b
Desmond Brand <dmnd@desmondbrand.com>
2015-11-07 21:02:15 -0800

    :art: fix indentation

--- __tests__/dedent-tests.js
+++ __tests__/dedent-tests.js
@@ -8,38 +8,38 @@ describe("dedent", () => {
   it("works without interpolation", () => {
     const dd = require("../dedent");
     const result = dd`first
-                    second
-                    third`;
+                      second
+                      third`;
     expect(result).toBe("first\nsecond\nthird");
   });
 
   it("works with interpolation", () => {
     const dd = require("../dedent");
     const result = dd`first ${"line"}
-                    ${"second"}
-                    third`;
+                      ${"second"}
+                      third`;
     expect(result).toBe("first line\nsecond\nthird");
   });
 
   it("works with suppressed newlines", () => {
     const dd = require("../dedent");
     const result = dd`first \
-                    ${"second"}
-                    third`;
+                      ${"second"}
+                      third`;
     expect(result).toBe("first second\nthird");
   });
 
   it("works with blank first line", () => {
     const dd = require("../dedent");
     const result = dd`
-      Some text that I might want to indent because:
+      Some text that I might want to indent:
         * reasons
         * fun
       That's all.
     `;
 
     expect(result).toBe(
-      "Some text that I might want to indent because:\n" +
+      "Some text that I might want to indent:\n" +
       "  * reasons\n" +
       "  * fun\n" +
       "That's all.");
@@ -58,10 +58,10 @@ describe("dedent", () => {
   it("works with removing same number of spaces", () => {
     const dd = require("../dedent");
     const result = dd`
-                    first
-                       second
-                          third
-                    `;
+                      first
+                         second
+                            third
+                     `;
     expect(result).toBe("first\nsecond\n   third");
   });
 
