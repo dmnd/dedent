805b3f0ac328694622e4a3dcc1271913fa8ec981
Desmond Brand <desmond@flexport.com>
2017-01-28 17:08:44 -0800

    Use snapshots for tests
    This might be overkill...

--- __tests__/dedent-tests.js
+++ __tests__/dedent-tests.js
@@ -4,105 +4,97 @@ import dd from '../dedent';
 
 describe("dedent", () => {
   it("works without interpolation", () => {
-    const result = dd`first
-                      second
-                      third`;
-    expect(result).toBe("first\nsecond\nthird");
+    expect(
+      dd`first
+         second
+         third`
+    ).toMatchSnapshot();
   });
 
   it("works with interpolation", () => {
-    const result = dd`first ${"line"}
-                      ${"second"}
-                      third`;
-    expect(result).toBe("first line\nsecond\nthird");
+    expect(
+      dd`first ${"line"}
+         ${"second"}
+         third`
+    ).toMatchSnapshot();
   });
 
   it("works with suppressed newlines", () => {
-    const result = dd`first \
-                      ${"second"}
-                      third`;
-    expect(result).toBe("first second\nthird");
+    expect(
+      dd`first \
+         ${"second"}
+         third`
+    ).toMatchSnapshot();
   });
 
   it("works with blank first line", () => {
-    const result = dd`
+    expect(dd`
       Some text that I might want to indent:
         * reasons
         * fun
       That's all.
-    `;
-
-    expect(result).toBe(
-      "Some text that I might want to indent:\n" +
-      "  * reasons\n" +
-      "  * fun\n" +
-      "That's all.");
+    `).toMatchSnapshot();
   });
 
   it("works with multiple blank first lines", () => {
-    const result = dd`
+    expect(
+      dd`
 
-                    first
-                    second
-                    third`;
-    expect(result).toBe("first\nsecond\nthird");
+         first
+         second
+         third`
+    ).toMatchSnapshot();
   });
 
   it("works with removing same number of spaces", () => {
-    const result = dd`
-                      first
-                         second
-                            third
-                     `;
-    expect(result).toBe("first\n   second\n      third");
+    expect(
+      dd`
+         first
+            second
+               third
+      `
+    ).toMatchSnapshot();
   });
 
   describe("single line input", () => {
-    const expected = "A single line of input.";
-
     it("works with single line input", () => {
-      const result = dd`A single line of input.`;
-      expect(result).toBe(expected);
+      expect(dd`A single line of input.`).toMatchSnapshot();
     });
 
     it("works with single line and closing backtick on newline", () => {
-      const result = dd`
+      expect(dd`
         A single line of input.
-      `;
-      expect(result).toBe(expected);
+      `).toMatchSnapshot();
     });
 
     it("works with single line and inline closing backtick", () => {
-      const result = dd`
-        A single line of input.`;
-      expect(result).toBe(expected);
+      expect(dd`
+        A single line of input.`
+      ).toMatchSnapshot();
     });
   });
 
   it("can be used as a function", () => {
-    const arg = `
+    expect(dd(`
       A test argument.
-    `;
-    expect(dd(arg)).toBe("A test argument.");
+    `)).toMatchSnapshot();
   });
 
   it("escapes backticks", () => {
-    expect(dd`\``).toBe('`');
+    expect(dd`\``).toMatchSnapshot();
   });
 
   it("doesn't strip exlicit newlines", () => {
-    const result = dd`
+    expect(dd`
       <p>Hello world!</p>\n
-    `;
-    expect(result).toBe("<p>Hello world!</p>\n");
+    `).toMatchSnapshot();
   });
 
   it("doesn't strip exlicit newlines with mindent", () => {
-    const result = dd`
+    expect(dd`
       <p>
         Hello world!
       </p>\n
-    `;
-    expect(result).toBe("<p>\n  Hello world!\n</p>\n");
+    `).toMatchSnapshot();
   });
 });
