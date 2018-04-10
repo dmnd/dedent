77673fef42c0374e744df868ba1b2829a5aa4781
Haroen Viaene <fingebimus@me.com>
2018-04-10 13:35:23 +0200

    feat(macro): add a /macro for babel-macros (#19)
    * feat(macro): add a /macro for babel-macros
    
    fixes #18
    
    cc @kentcdodds
    
    * :art: default prettier

---
+++ __tests__/dedent-macro-test.js
@@ -0,0 +1,66 @@
+import pluginTester from "babel-plugin-tester";
+import plugin from "babel-plugin-macros";
+
+pluginTester({
+  plugin,
+  snapshot: true,
+  babelOptions: { filename: __filename },
+  tests: [
+    {
+      title: "as a function",
+      code: `
+        import dedent from "../macro";
+
+        dedent(\`
+          some stuff
+        \`);
+      `
+    },
+    {
+      title: "as a template string",
+      code: `
+        import dedent from "../macro";
+
+        dedent\`
+          some stuff
+        \`;
+      `
+    },
+    {
+      title: "explicit newline",
+      code: `
+        import dedent from "../macro";
+
+        dedent\`
+          <p>
+            Hello world!
+          </p>\n
+        \`;
+      `
+    },
+    {
+      title: "multiple indentations",
+      code: `
+        import dedent from "../macro";
+
+        dedent\`
+            first
+              second
+                  third
+        \`;
+      `
+    },
+    {
+      title: "expressions",
+      code: `
+        import dedent from "../macro";
+
+        dedent\`
+          first ${"line"}
+            ${"second"}
+            third
+        \`;
+      `
+    }
+  ]
+});
