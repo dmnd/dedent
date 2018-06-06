c039a00768b0a7fd50213adc3d5d37e1250de8cc
Michiel Dral <m.c.dral@gmail.com>
2018-06-07 05:56:29 +1000

    Generate new dist build

--- dist/dedent.js
+++ dist/dedent.js
@@ -10,16 +10,35 @@ function dedent(strings) {
 
   // first, perform interpolation
   var result = "";
+
+  for (var _len = arguments.length, values = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+    values[_key - 1] = arguments[_key];
+  }
+
   for (var i = 0; i < raw.length; i++) {
-    result += raw[i].
+    result += raw[i]
     // join lines when there is a suppressed newline
-    replace(/\\\n[ \t]*/g, "").
-
+    .replace(/\\\n[ \t]*/g, "")
     // handle escaped backticks
-    replace(/\\`/g, "`");
+    .replace(/\\`/g, "`");
 
-    if (i < (arguments.length <= 1 ? 0 : arguments.length - 1)) {
-      result += arguments.length <= i + 1 ? undefined : arguments[i + 1];
+    if (i < values.length) {
+      // Over-indent multiline interpolations so they don't 'fall' to 0
+      if (values[i].includes('\n')) {
+        var spaces_before_match = result.match(/(?:^|\n)( *)$/);
+        if (spaces_before_match && typeof values[i] === 'string') {
+          (function () {
+            var spaces_before = spaces_before_match[1];
+            result += values[i].split('\n').map(function (str, i) {
+              return i === 0 ? str : "" + spaces_before + str;
+            }).join('\n');
+          })();
+        } else {
+          result += values[i];
+        }
+      } else {
+        result += values[i];
+      }
     }
   }
 
@@ -40,17 +59,15 @@ function dedent(strings) {
   });
 
   if (mindent !== null) {
-    (function () {
-      var m = mindent; // appease Flow
-      result = lines.map(function (l) {
-        return l[0] === " " ? l.slice(m) : l;
-      }).join("\n");
-    })();
+    var m = mindent; // appease Flow
+    result = lines.map(function (l) {
+      return l[0] === " " ? l.slice(m) : l;
+    }).join("\n");
   }
 
-  return result.
+  return result
   // dedent eats leading and trailing whitespace too
-  trim().
+  .trim()
   // handle escaped newlines at the end to ensure they don't get stripped too
-  replace(/\\n/g, "\n");
+  .replace(/\\n/g, "\n");
 }
