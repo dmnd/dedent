afbe6d233c1316c33e4d8f04991d2cf12d0ccb97
adrianjost <22987140+adrianjost@users.noreply.github.com>
2020-01-17 17:42:22 +0100

    strip comments from build

--- dist/dedent-tabs.js
+++ dist/dedent-tabs.js
@@ -6,21 +6,16 @@ Object.defineProperty(exports, "__esModule", {
 exports["default"] = dedent;
 
 function dedent(strings) {
-  // $FlowFixMe: Flow doesn't undestand .raw
-  var raw = typeof strings === "string" ? [strings] : strings.raw; // first, perform interpolation
-
+  var raw = typeof strings === "string" ? [strings] : strings.raw;
   var result = "";
 
   for (var i = 0; i < raw.length; i++) {
-    result += raw[i] // join lines when there is a suppressed newline
-    .replace(/\\\n[ \t]*/g, "") // handle escaped backticks
-    .replace(/\\`/g, "`");
+    result += raw[i].replace(/\\\n[ \t]*/g, "").replace(/\\`/g, "`");
 
     if (i < (arguments.length <= 1 ? 0 : arguments.length - 1)) {
       result += i + 1 < 1 || arguments.length <= i + 1 ? undefined : arguments[i + 1];
     }
-  } // now strip indentation
-
+  }
 
   var lines = result.split("\n");
   var mindent = null;
@@ -31,7 +26,6 @@ function dedent(strings) {
       var indent = m[1].length;
 
       if (!mindent) {
-        // this is the first indented line
         mindent = indent;
       } else {
         mindent = Math.min(mindent, indent);
@@ -40,16 +34,12 @@ function dedent(strings) {
   });
 
   if (mindent !== null) {
-    var m = mindent; // appease Flow
-
-    var whitespaceCharacters = [" ", "\t"]; // [space, tab]
-
+    var m = mindent;
+    var whitespaceCharacters = [" ", "\t"];
     result = lines.map(function (l) {
       return whitespaceCharacters.includes(l[0]) ? l.slice(m) : l;
     }).join("\n");
   }
 
-  return result // dedent eats leading and trailing whitespace too
-  .trim() // handle escaped newlines at the end to ensure they don't get stripped too
-  .replace(/\\n/g, "\n");
+  return result.trim().replace(/\\n/g, "\n");
 }
