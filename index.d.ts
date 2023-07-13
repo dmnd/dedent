83a7fbbbeabcf2b998f1488ea803428ecf31152e
Gareth Jones <Jones258@Gmail.com>
2023-07-14 10:03:36 +1200

    fix: update types

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,2 @@
-export default function dedent(strings: TemplateStringsArray, ...values: Array<string>): string;
+export default function dedent(literals: string): string;
+export default function dedent(strings: TemplateStringsArray, ...values: Array<any>): string;
