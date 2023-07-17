7d735a5ad065d8041badc00fa9d813fa01a37855
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2023-07-17 11:14:17 -0400

    build: convert dedent.ts and tests to TypeScript (#51)
    Fixes #50.
    
    Integrates with a few tools:
    
    * TypeScript itself for type checking (`yarn tsc`)
    * [Babel's TypeScript
    preset](https://babeljs.io/docs/babel-preset-typescript) to stick with
    the existing output process (`yarn build:legacy`, `yarn build:modern`)
    * [tsup](https://tsup.egoist.dev) to generate a single `.d.ts` from
    `dedent.ts` (`yarn build:types`)
    
    Types are moved from `index.d.ts` to `dist/dedent.d.ts` to match the
    other `dist/*` files. I confirmed with [the `attw`
    cli](https://github.com/arethetypeswrong/arethetypeswrong.github.io/tree/e33adb4d894b041a51b8366bf0611ff5ecaf38cb/packages/cli)
    that these support various Node resolution strategies.
    
    `macro.js` and `macro.d.ts` are untouched, per #50 -> #52.

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+/* eslint-env node */
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/strict-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+  ],
+};
